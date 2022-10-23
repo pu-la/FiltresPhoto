2c7adfb59d36b0feef633fc79059cd6ec3312a22
Rienk Kuipers <rienk@kuipers.org>
2022-10-23 13:48:18 +0200

    Updated README; Added Filtre Gris; Fixed filtres to work after eachother

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from PIL import Image
 import math
 # On en aura probablement besoin
 
-# VARS
+# VARS (init)
 
 # FONCTIONS
 
 def filtre_float(in_image, coeff_liste):
     """Donne une image et des coefficients pour les couleurs sous forme de liste (Tout) - supporte FLOAT pour des coeffs"""
-    image = Image.open(in_image).copy() # Plus facile pour travailler dessus
+    image = in_image # Plus facile pour travailler dessus
     for y in range(image.height):       # Balailler l'y de l'image 
         for x in range(image.width):    # Balailler le x de l'image
             image.putpixel((x, y),(int(round(image.getpixel((x, y))[0]*coeff_liste[0], 0)), int(round(image.getpixel((x, y))[1]*coeff_liste[1], 0)), int(round(image.getpixel((x, y))[2]*coeff_liste[2], 0))))    # Magique.
@@ -19,7 +19,7 @@ def filtre_float(in_image, coeff_liste):
 
 def filtre(in_image, coeff_liste):
     """Donne une image et des coefficients pour les couleurs sous forme de liste (Que des entires!) - ne supporte PAS les floats."""
-    image = Image.open(in_image).copy() # Plus facile pour travailler dessus
+    image = in_image # Plus facile pour travailler dessus
     for y in range(image.height):       # Balailler l'y de l'image 
         for x in range(image.width):    # Balailler le x de l'image
             image.putpixel((x, y),(image.getpixel((x, y))[0]*coeff_liste[0], image.getpixel((x, y))[1]*coeff_liste[1], image.getpixel((x, y))[2]*coeff_liste[2]))    # Magique.
@@ -33,4 +33,14 @@ def filtre_g(in_image):
 def filtre_b(in_image):
     '''Filtre Bleu, passe une image'''
     return filtre(in_image, [0,0,1])
+def filtre_gris(in_image):
+    image = in_image # Plus facile pour travailler dessus
+    for y in range(image.height):       # Balailler l'y de l'image 
+        for x in range(image.width):    # Balailler le x de l'image
+            moyenne = int(round((image.getpixel((x, y))[0]+image.getpixel((x, y))[1]+image.getpixel((x, y))[2])/3, 0))
+            image.putpixel((x, y), (moyenne, moyenne, moyenne))    # Magique.
+    return image
+image = Image.open("maison.jpeg")
+filtre_gris(image).show()
+
 #PROGRAMME
